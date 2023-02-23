726a9aa9c2c22d559561cd8144234be2795b19dd
arnaud <nakarnaud@gmail.com>
2023-02-23 04:33:18 -0800

    0-main.cfile

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests function that prints if intger is postive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_neagtive(i);
+	return (0);
+}
