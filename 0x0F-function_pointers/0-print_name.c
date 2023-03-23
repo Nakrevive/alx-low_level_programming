efe409b27afd76d0085505b185b652e57ac5f5b2
arnaud <nakarnaud@gmail.com>
2023-03-23 01:11:54 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_name.c -o a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
-/**
- * main - a function that prints name
- * Return: 0 (success)
- */
-void print_name(char *name, void (*f)(char *))
-{
-	
+#ifndef FUNCTION_POINTERS_H
+	#define FUNCTION_POINTERS_H
+
+
+	#include <stddef.h>
+	#include <stdlib.h>
+
+
+	void print_name(char *name, void (*f)(char *));
+	int _putchar(char c);
+	void array_iterator(int *array, size_t size, void (*action)(int));
+	int int_index(int *array, int size, int (*cmp)(int));
+
+
+	#endif
+
